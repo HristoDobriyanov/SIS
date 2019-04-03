2cc473a9fd6ba38054e7cf44c162926a7e077493
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-04-03 16:01:59 +0300

    Updates

--- SIS.Http/Requests/HttpRequest.cs
+++ SIS.Http/Requests/HttpRequest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using SIS.Http.Common;
 using SIS.Http.Contracts;
 using SIS.Http.Enum;
+using SIS.Http.Exceptions;
 using SIS.Http.Headers;
 
 namespace SIS.Http.Requests
@@ -24,11 +27,125 @@ namespace SIS.Http.Requests
 
         private void ParseRequest(string requestString)
         {
+            string[] splitRequestContent = requestString
+                .Split(Environment.NewLine, StringSplitOptions.None);
+
+            string[] requestLine = splitRequestContent[0].Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            if (!this.ValidateRequestLine(requestLine))
+            {
+                throw new BadRequestException();
+            }
+
+
+            this.ParseRequestMethod(requestLine);
+            this.ParseRequestUrl(requestLine);
+            this.ParseRequestPath(requestLine);
+
+            this.ParseHeaders(splitRequestContent.Skip(1).ToArray());
+            bool requestHasBody = splitRequestContent.Length > 1;
+            this.ParseRequestParameters(splitRequestContent[splitRequestContent.Length - 1], requestHasBody);
+
+
+
+        }
+
+        private void ParseRequestParameters(string bodyParameters, bool requestHasBody)
+        {
+            this.ParseQueryParameters(this.Url);
+
+            if (requestHasBody)
+            {
+                this.ParseFormDataParameters(bodyParameters);
+            }
+        }
+
+        private void ParseFormDataParameters(string bodyParameters)
+        {
             throw new NotImplementedException();
         }
 
+        private void ParseQueryParameters(string url)
+        {
+            var queryParameters = this.Url?.Split(new[] { '?', '#' }).Skip(1);
+        }
+
+        private void ParseHeaders(string[] requestHeaders)
+        {
+            if (!requestHeaders.Any())
+            {
+                throw new BadRequestException();
+            }
+
+            foreach (var requestHeader in requestHeaders)
+            {
+                if (string.IsNullOrEmpty(requestHeader))
+                {
+                    return;
+                }
+
+                var splitRequestHeader = requestHeader.Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                var requestHeaderKey = splitRequestHeader[0];
+                var requestHeaderValue = splitRequestHeader[1];
+
+                this.Headers.Add(new HttpHeader(requestHeaderKey, requestHeaderValue));
+            }
+        }
+
+        private void ParseRequestPath(string[] requestLine)
+        {
+            var path = this.Url?.Split('?').FirstOrDefault();
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new BadRequestException();
+            }
+
+            this.Path = path;
+        }
+
+        private void ParseRequestUrl(string[] requestLine)
+        {
+            if (string.IsNullOrEmpty(requestLine[1]))
+            {
+                throw new BadRequestException();
+            }
+
+            this.Url = requestLine[1];
+
+        }
+
+        private void ParseRequestMethod(string[] requestLine)
+        {
+
+            var parseResult = System.Enum.TryParse<HttpRequestMethod>
+                (requestLine[0], out var parsedRequestMethod);
+
+            if (!parseResult)
+            {
+                throw new BadRequestException();
+            }
+
+            this.RequestMethod = parsedRequestMethod;
+
+        }
+
+        private bool ValidateRequestLine(string[] requestLine)
+        {
+            if (!requestLine.Any())
+            {
+                throw new BadRequestException();
+            }
 
 
+            if (requestLine.Length == 3 && requestLine[2] == GlobalConstants.HttpOneProtocolFragment)
+            {
+                return true;
+            }
+
+            return false;
+        }
 
 
 
