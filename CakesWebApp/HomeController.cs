44c92a84c31931e8661a4410c79755ee186d5b4b
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-04-10 23:48:55 +0300

    Updates

---
+++ CakesWebApp/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using SIS.Http.Responses.Contracts;
+using SIS.WebServer.Results;
+
+namespace CakesWebApp
+{
+    public class HomeController
+    {
+        public IHttpResponse Index()
+        {
+            string content = "<h1>Hello World!</h1>";
+
+            return new HtmlResult(content, HttpStatusCode.OK);
+        }
+    }
+}
---
+++ CakesWebApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SIS.Demo;
+using SIS.Http.Enum;
+using SIS.WebServer;
+using SIS.WebServer.Routing;
+
+namespace CakesWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Routes[HttpRequestMethod.GET]["/"] = request => new HomeController().Index();
+
+            Server server = new Server(80, serverRoutingTable);
+
+            server.Run();
+        }
+    }
+}
